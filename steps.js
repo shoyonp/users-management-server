49ab7cb8423da095952f7b4cd4156303d32bbf8a
shoyonp <shoyonkumar143@gmail.com>
2024-11-24 03:45:59 +0600

    het data from clint side done

---
+++ steps.js
@@ -0,0 +1,9 @@
+/**
+ * 1. creat a post api on the server site
+ * 2. client side send data via post
+ * 3. set fetch method inside the fetch option (second parameter)
+ * 4. options will have three things : method
+ * 5. options will have header : 'content-type':'application/json'
+ * 6. don't forget to send data by JSON.Stringify in the body:
+ * 7. On the server side don't forget to use express.json() middleWare
+ */
